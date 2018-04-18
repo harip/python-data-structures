1bda96de61be1c3c2378c85efaf4343e3bc73c2f
Hari Pulugurta <charanp@gmail.com>
2018-04-18 12:11:19 -0400

    minor refactor

--- generaltree/plot_tree.py
+++ generaltree/plot_tree.py
@@ -12,7 +12,7 @@ class PlotTree:
     def __init__(self):
         plt.rcdefaults()
         self.fig, self.ax = plt.subplots()
-        self.grid = np.mgrid[0.2:0.8:3j, 0.2:0.8:3j].reshape(2, -1).T
+        self.grid = None
         self.patches = []
         self.tree=None
         self.plotted=dict()
@@ -21,19 +21,27 @@ class PlotTree:
         y = xy[1] - 0.15  # shift y-value for label so that it's below the artist
         plt.text(xy[0], y, text, ha="center", family='sans-serif', size=14)
     
-    def get_mgrid(self):
+    def set_mgrid(self):
         num_of_paths=len(self.tree.paths)
         max_node_path= self.tree.get_height()+1
 
         grid_size_y=complex(0,max_node_path)
         grid_size_x=complex(0,num_of_paths)
-        tree_grid=np.mgrid[0.2:0.8:grid_size_x, 0.2:0.8:grid_size_y].reshape(2, -1).T
-        return tree_grid
+        self.grid =np.mgrid[0.2:0.8:grid_size_x, 0.2:0.8:grid_size_y].reshape(2, -1).T
+
+    def set_plot(self):
+        colors = np.linspace(0, 1, len(self.patches ))
+        collection = PatchCollection(self.patches , cmap=plt.cm.hsv, alpha=0.3)
+        collection.set_array(np.array(colors))
+        self.ax.add_collection(collection)
+        plt.axis('equal')
+        plt.axis('off')
+        plt.tight_layout()
+        plt.show()                
     
     def plot(self,tree):
         self.tree=tree
-        plot_grid=self.get_mgrid()
-        patches = []
+        self.set_mgrid()
         tree_height=self.tree.get_height()+1
 
         path_counter=0
@@ -45,23 +53,14 @@ class PlotTree:
                     path_node_counter=path_node_counter+1
                     continue
 
-                ellipse = mpatches.Ellipse(plot_grid[path_node_counter], 0.2, 0.1)
-                patches.append(ellipse) 
-                self.label(plot_grid[path_node_counter], path_node_counter)  
-                path_node_counter=path_node_counter+1
-                
+                ellipse = mpatches.Ellipse(self.grid[path_node_counter], 0.2, 0.1)
+                self.patches.append(ellipse) 
+                self.label(self.grid[path_node_counter], path_node_counter)  
+
+                path_node_counter=path_node_counter+1                
                 self.plotted[j]=True
 
             path_counter=path_counter+1
             path_node_counter=tree_height*path_counter
- 
-        colors = np.linspace(0, 1, len(patches))
-        collection = PatchCollection(patches, cmap=plt.cm.hsv, alpha=0.3)
-        collection.set_array(np.array(colors))
-        self.ax.add_collection(collection)
 
-        plt.axis('equal')
-        plt.axis('off')
-        plt.tight_layout()
-
-        plt.show()                
+        self.set_plot()

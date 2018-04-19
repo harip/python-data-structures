935461c5ccecedc29851b4cb77c332a970c721e6
Hari Pulugurta <charanp@gmail.com>
2018-04-18 23:13:45 -0400

    cleanup code

--- tests/test_generaltree.py
+++ tests/test_generaltree.py
@@ -7,101 +7,61 @@ def test_tree_plot():
     t=Tree()
 
     # Add a parent node
-    n=NodeInfo()
-    n.node_key="Html"
-    n.type=NodeType.ROOT    
+    n=NodeInfo("Html",None,NodeType.ROOT) 
     t.add_node(n,None)
 
     # Add a child node
-    cn=NodeInfo()
-    cn.node_key="head"
+    cn=NodeInfo("head",None,None) 
     t.add_node(cn,n)
 
     # Add a child node
-    cn5=NodeInfo()
-    cn5.node_key="js"
+    cn5=NodeInfo("js",None,None) 
     t.add_node(cn5,cn)    
 
     # # Add a child node
-    cn6=NodeInfo()
-    cn6.node_key="stylesheet"
+    cn6=NodeInfo("stylesheet",None,None) 
     t.add_node(cn6,cn)       
 
     # # Add a child node
-    cn1=NodeInfo()
-    cn1.node_key="body"
+    cn1=NodeInfo("body",None,None) 
     t.add_node(cn1,n)    
 
     # # Add a child node to body
-    cn2=NodeInfo()
-    cn2.node_key="<p>"
+    cn2=NodeInfo("<p>",None,None) 
     t.add_node(cn2,cn1)        
 
     # Add a child node to body
-    cn3=NodeInfo()
-    cn3.node_key="<p>"
+    cn3=NodeInfo("<p>",None,None)
     t.add_node(cn3,cn1)     
 
     # Add a child node to body
-    cn4=NodeInfo()
-    cn4.node_key="<p>"
+    cn4=NodeInfo("<p>",None,None)
     t.add_node(cn4,cn1)     
  
      # Add a child node to body
-    cn7=NodeInfo()
-    cn7.node_key="<b>"
+    cn7=NodeInfo("<b>",None,None)
     t.add_node(cn7,cn4)    
 
+    assert t.height==3,"Tree height should be 2"
     t.plot_tree()
-
-def test_tree():
-    t=Tree()
-
-    # Add a parent node
-    n=NodeInfo()
-    n.node_key="Html"
-    n.type=NodeType.ROOT    
-    t.add_node(n,None)
-
-    # Add a child node
-    cn=NodeInfo()
-    cn.node_key="head"
-    t.add_node(cn,n)
-
-    # Add a child node
-    cn2=NodeInfo()
-    cn2.node_key="js"
-    t.add_node(cn2,cn)    
-
-    # # Add a child node
-    cn3=NodeInfo()
-    cn3.node_key="body"
-    t.add_node(cn3,n)    
-
-    assert t.height==2,"Tree height should be 2"
 
 def test_tree_nodes():
     t=Tree()
 
     # Add a parent node
-    n=NodeInfo()
-    n.node_key="Html"
-    n.type=NodeType.ROOT    
+    n=NodeInfo("Html",None,NodeType.ROOT)
     t.add_node(n,None)
 
     # Add a child node
-    cn=NodeInfo()
-    cn.node_key="head"
+    cn=NodeInfo("head",None,None)
     t.add_node(cn,n)
 
     # Add a child node
-    cn2=NodeInfo()
-    cn2.node_key="js"
+    cn2=NodeInfo("js",None,None)
     t.add_node(cn2,cn)    
 
     # # Add a child node
-    cn3=NodeInfo()
-    cn3.node_key="body"
+    cn3=NodeInfo("body",None,None)
     t.add_node(cn3,n)    
 
     assert len(t.get_all_nodes())==4
@@ -110,14 +70,11 @@ def test_tree_get_node():
     t=Tree()
 
     # Add a parent node
-    n=NodeInfo()
-    n.node_key="Html"
-    n.type=NodeType.ROOT    
+    n=NodeInfo("Html",None,NodeType.ROOT)
     t.add_node(n,None)
 
     # Add a child node
-    cn=NodeInfo()
-    cn.node_key="head"
+    cn=NodeInfo("head",None,None)
     t.add_node(cn,n)
 
     node=t.get_node("head")
@@ -125,4 +82,4 @@ def test_tree_get_node():
     assert node.node_key=="head","head node should be returned"
 
 if __name__=="__main__":
-    test_tree_get_node()
+    test_tree_plot()
--- tree_ds/node_info.py
+++ tree_ds/node_info.py
@@ -2,9 +2,9 @@ import uuid,sys,os
 from . import node_type
 
 class NodeInfo:
-    def __init__(self):
+    def __init__(self,key,val,ntype):
         self.id=str(uuid.uuid4())
         self.parent_id=self.id
-        self.node_key=""
-        self.node_val="" 
-        self.type=node_type.NodeType.CHILD
+        self.node_key=key
+        self.node_val=val 
+        self.type= node_type.NodeType.CHILD if ntype is None else ntype
